d03f2053115f2533259809610ace20a91d705e4c
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-05-19 22:24:33 +0100

    feat: Create categories and category components

---
+++ src/components/categories/categories.jsx
@@ -0,0 +1,13 @@
+import "./categories.scss";
+import CategoryItem from "../category-item/category-item";
+
+const CategoriesList = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+export default CategoriesList;
