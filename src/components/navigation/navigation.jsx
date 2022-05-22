ea42822aad447268bad66f81b4e731fcab8c6635
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-05-22 21:50:15 +0100

    Add authentication and create user in firebase

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <nav className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
